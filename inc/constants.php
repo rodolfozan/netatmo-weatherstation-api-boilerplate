1014182de1409ffb2591a630a07b282386860351
rodolfozan <64145392+rodolfozan@users.noreply.github.com>
2020-04-23 12:01:31 +0200

    Create constants.php

---
+++ inc/constants.php
@@ -0,0 +1,10 @@
+<?php
+
+// Constants
+	const USERNAME		= "rolzan.rz@gmail.com";
+	const PASSWORD		= "5Cleopatra11#";
+	const APPID			= "59e5a4184deddb623a8b4d67";
+	const APPSECRET     = "TcLRpMpWisDZFJ2MZKA92po1UcRXtyXb0T";
+	const LANGUAGE      = "de";
+
+?>
