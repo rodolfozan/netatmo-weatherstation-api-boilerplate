afea0ded5e7349bb73f02ed3d2fd77857bb0b636
rodolfozan <64145392+rodolfozan@users.noreply.github.com>
2020-04-23 12:02:38 +0200

    Update language.php

--- classes/language.php
+++ classes/language.php
@@ -1,1 +1,17 @@
+<?php 
 
+class language {
+
+   public $data;
+
+   function __construct($language) {
+      $data = file_get_contents("lang/" . $language . ".json");
+      $this->data = json_decode($data);
+   }
+
+   function translate() {
+        return $this->data;
+   }
+}
+
+?>
